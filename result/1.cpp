acc2a50fb5bbbe08ea308e3d5eff1f7e79335069
riaapatel28 <kakadiyariya03@gmail.com>
2026-01-23 23:23:22 +0530

    Add files via upload

---
+++ result/1.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+   string name,a;
+   name="charotar university of scirnce and technology";
+   a="semester gread report";
+   cout<<setw(80)<<setfill(' ')<<name<<endl;
+   cout<<setw(67)<<setfill(' ')<<a<<endl;
+   cout<<" "<<endl;
+   cout<<" "<<endl;
+   cout<<"Name of stdudent : Riya Kakadiya vasantbhai "<<setw(50)<<"ID no. : 25CE043"<<endl;
+   cout<<"Programme : B.TECH.(Computer)"<<setw(62)<<"Semester : 01"<<endl;
+   cout<<"Month & year : Dec-25"<<endl;
+   cout<<" "<<endl;
+   cout<<"_____________________________________________________________________________________________________________________________________________"<<endl;
+   cout<<setw(35)<<"course"<<setw(35)<<"|"<<setw(8)<<"credit"<<setw(5)<<"|"<<setw(5)<<"cce"<<setw(5)<<"|"<<setw(5)<<"see"<<setw(5)<<"|"<<setw(5)<<"cce"<<setw(5)<<"|"<<setw(5)<<"see"<<setw(5)<<"|"<<setw(13)<<"agg. grade"<<setw(5)<<"|"<<endl;
+   cout<<"---------------------------------------------------------------------------------------------------------------------------------------------"<<endl;
+   cout<<setw(2)<<"CEUC101  COMPUTER CONSEPT AND PROGRAMING"<<setw(30)<<"|"<<setw(7)<<"5.00"<<setw(6)<<"|"<<setw(5)<<"c"<<setw(5)<<"|"<<setw(5)<<"B"<<setw(5)<<"|"<<setw(5)<<"A"<<setw(5)<<"|"<<setw(5)<<"C"<<setw(5)<<"|"<<setw(5)<<"B"<<setw(13)<<"|"<<endl;
+   cout<<setw(2)<<"EEUD101	BASICS OF ELECTRONICS AND ELECTRICAL ENGINEERING"<<setw(14)<<"|"<<setw(7)<<"4.00"<<setw(6)<<"|"<<setw(5)<<"B+"<<setw(5)<<"|"<<setw(5)<<"A+"<<setw(5)<<"|"<<setw(5)<<"A+"<<setw(5)<<"|"<<setw(5)<<"B+"<<setw(5)<<"|"<<setw(5)<<"A"<<setw(13)<<"|"<<endl;
+   cout<<setw(2)<<"FTUC101	FOUNDATION COURSE ON MATHEMATICS AND PHYSICS (AUDIT COURSE)"<<setw(3)<<"|"<<setw(7)<<"-"<<setw(6)<<"|"<<setw(5)<<"-"<<setw(5)<<"|"<<setw(5)<<"-"<<setw(5)<<"|"<<setw(5)<<"-"<<setw(5)<<"|"<<setw(5)<<"A"<<setw(5)<<"|"<<setw(5)<<"A"<<setw(13)<<"|"<<endl;
+   cout<<setw(2)<<"HSUA101	COMMUNICATIVE ENGLISH"<<setw(41)<<"|"<<setw(7)<<"2.00"<<setw(6)<<"|"<<setw(5)<<"-"<<setw(5)<<"|"<<setw(5)<<"-"<<setw(5)<<"|"<<setw(5)<<"A"<<setw(5)<<"|"<<setw(5)<<"A"<<setw(5)<<"|"<<setw(5)<<"A"<<setw(13)<<"|"<<endl;
+   cout<<setw(2)<<"ITUS101	ICT WORKSHOP"<<setw(50)<<"|"<<setw(7)<<"2.00"<<setw(6)<<"|"<<setw(5)<<"-"<<setw(5)<<"|"<<setw(5)<<"-"<<setw(5)<<"|"<<setw(5)<<"A"<<setw(5)<<"|"<<setw(5)<<"B+"<<setw(5)<<"|"<<setw(5)<<"B+"<<setw(13)<<"|"<<endl;
+   cout<<setw(2)<<"MSUD101	ENGINEERING MATHEMATICS-I"<<setw(37)<<"|"<<setw(7)<<"4.00"<<setw(6)<<"|"<<setw(5)<<"B+"<<setw(5)<<"|"<<setw(5)<<"B"<<setw(5)<<"|"<<setw(5)<<"-"<<setw(5)<<"|"<<setw(5)<<"-"<<setw(5)<<"|"<<setw(5)<<"B"<<setw(13)<<"|"<<endl;
+   cout<<setw(2)<<"PSUD101	ENGINEERING PHYSICS-I"<<setw(41)<<"|"<<setw(7)<<"2.00"<<setw(6)<<"|"<<setw(5)<<"-"<<setw(5)<<"|"<<setw(5)<<"-"<<setw(5)<<"|"<<setw(5)<<"A+"<<setw(5)<<"|"<<setw(5)<<"B"<<setw(5)<<"|"<<setw(5)<<"B+"<<setw(13)<<"|"<<endl;
+   cout<<"_____________________________________________________________________________________________________________________________________________"<<endl;
+   cout<<setw(7)<<"Semester Performance"<<setw(25)<<"|"<<setw(7)<<"Total Credits"<<setw(25)<<"|"<<setw(7)<<"Credits Earned"<<setw(25)<<"|"<<setw(10)<<"SGPA"<<setw(10)<<endl;
+   cout<<setw(7)<<"                    "<<setw(25)<<"|"<<setw(7)<<"19.00"<<setw(31)<<"|"<<setw(7)<<"19.00"<<setw(32)<<"|"<<setw(10)<<"6.84"<<setw(10)<<endl;
+   cout<<"_____________________________________________________________________________________________________________________________________________"<<endl;
+
+   cout<<" "<<endl;
+
+}
+
+
+
